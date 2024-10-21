995ad50865855b1770ccaa09ee4b59d3b30321f1
24lp5308@nest.edu.mn <24lp5308nest.edu.mn@G305-CC0036.local>
2024-10-21 13:00:31 +0800

    typescript

---
+++ src/component/User.tsx
@@ -0,0 +1,19 @@
+import { Dispatch, SetStateAction } from "react";
+
+type Person={
+    user:string;
+    setUser:Dispatch<SetStateAction<string>>;
+    handleClickOne:()=>void;
+}
+export const Users=({user, setUser, handleClickOne}:Person)=>{
+    const handleClick=()=>{
+        setUser("user")
+    }
+    return(
+        <div>
+            <div>{user}</div>
+            <button onClick={handleClick} type="submit"> Click 1</button>
+            <button onClick={handleClickOne} type="submit">Click2</button>
+        </div>
+    )
+}
